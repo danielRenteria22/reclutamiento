fcf468162a6edcdfee7f791fcd792e9754481a37
Eduardo Alvarado <30761895+GM-Chakal@users.noreply.github.com>
2018-12-24 11:59:54 -0700

    Add files via upload

--- reclutamiento-master/candidato/lado-oscuro/quest.php
+++ reclutamiento-master/candidato/lado-oscuro/quest.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Cuestionario de Inicio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../../style.css">
 </head>
 <body>
     <left><h1>Cuestionario Inicial</h1></left>
