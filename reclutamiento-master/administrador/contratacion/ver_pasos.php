707188fb04089e233dacc2ef975e5b42f266e4d0
danielRenteria22 <32469635+danielRenteria22@users.noreply.github.com>
2018-12-24 11:48:02 -0700

    Update ver_pasos.php

--- reclutamiento-master/administrador/contratacion/ver_pasos.php
+++ reclutamiento-master/administrador/contratacion/ver_pasos.php
@@ -1,4 +1,8 @@
 <html>
+    
+    <head>
+       <link rel="stylesheet" type="text/css" href="../../style.css">
+    </head>
     <body>
         <center><h1> Pasos para requisisiones</h1></center>
         <?php
