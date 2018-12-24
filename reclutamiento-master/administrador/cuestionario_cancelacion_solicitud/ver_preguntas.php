e36be0f76d068e6e46799b79671711fd7c58c74b
danielRenteria22 <32469635+danielRenteria22@users.noreply.github.com>
2018-12-24 11:49:37 -0700

    Update ver_preguntas.php

--- reclutamiento-master/administrador/cuestionario_cancelacion_solicitud/ver_preguntas.php
+++ reclutamiento-master/administrador/cuestionario_cancelacion_solicitud/ver_preguntas.php
@@ -1,4 +1,7 @@
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../../style.css">
+    </head>
     <body>
         <center><h1>Cuestionario para cancelacion de solicitud</h1></center>
         <?php
