f07f2f133286237ba6faccff9d600c2996abadf5
danielRenteria22 <32469635+danielRenteria22@users.noreply.github.com>
2018-12-24 12:00:58 -0700

    Update new_func_part.php

--- reclutamiento-master/usuario/perfiles/new_func_part.php
+++ reclutamiento-master/usuario/perfiles/new_func_part.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Crear Funcion Particular</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../../style.css">
 </head>
 <body>
     <center><h1>Crear Funcion Particular</h1></center>
