453d7c51d3e1db1c561d920f5cfd70e7a76c2ad4
Eduardo Alvarado <30761895+GM-Chakal@users.noreply.github.com>
2018-12-31 17:25:22 -0700

    Add files via upload

---
+++ reclutamiento-master/administrador/mercado/ver_mercado.php
@@ -0,0 +1,144 @@
+<?php 
+    include "../../verificacion.php";
+    verificar();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../../style.css">
+    <title>Usarios</title>
+</head>
+<body>
+<?php
+    include '../../config.php';
+    $conn=mysqli_connect($host,$user,$pass,$name);
+    if(mysqli_connect_errno($conn))
+    {
+        echo 'No se pudo hacer la conexión con la base de datos';
+        exit;
+    }
+    echo"<h3>Mercado Interno</h3>";
+    echo "<table border = 1>
+    <tr>
+        <th>No.</th>
+        <th>Usuario</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Estado Civil</th>
+        <th>Correo</th>
+        <th>Telefono</th>
+        <th>NSS</th>
+    </tr>";
+    $query = "SELECT * FROM employees where tipo = '3'";
+    $c=1;
+    $result = mysqli_query($conn, $query);
+    while($row = mysqli_fetch_array($result)){
+        $email = $row["email"];
+        $nombre = $row["empfullname"];
+        $apellidos = $row["apellidos"];
+        $username = $row["employid"];
+        $civ = $row["edociv"];
+        $phone = $row["telefono"];
+        $nss = $row["nss"];
+        echo "<tr>
+                <td>$c</td>
+                <td>$username</td>
+                <td>$nombre</td>
+                <td>$apellidos</td>
+                <td>$civ</td>
+                <td>$email</td>
+                <td>$phone</td>
+                <td>$nss</td>
+            </tr>";
+        $c++;
+    }
+    echo "</table>";
+    echo "</br>";
+
+
+    echo"<h3>Mercado Externo</h3>";
+    echo "<table border = 1>
+    <tr>
+        <th>No.</th>
+        <th>Usuario</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Estado Civil</th>
+        <th>Correo</th>
+        <th>Telefono</th>
+        <th>NSS</th>
+    </tr>";
+    $query = "SELECT * FROM employees where tipo = '0'";
+    $c=1;
+    $result = mysqli_query($conn, $query);
+    while($row = mysqli_fetch_array($result)){
+        $email = $row["email"];
+        $nombre = $row["empfullname"];
+        $apellidos = $row["apellidos"];
+        $username = $row["employid"];
+        $civ = $row["edociv"];
+        $phone = $row["telefono"];
+        $nss = $row["nss"];
+        echo "<tr>
+                <td>$c</td>
+                <td>$username</td>
+                <td>$nombre</td>
+                <td>$apellidos</td>
+                <td>$civ</td>
+                <td>$email</td>
+                <td>$phone</td>
+                <td>$nss</td>
+            </tr>";
+        $c++;
+    }
+    echo "</table>";
+    echo "</br>";
+
+    echo"<h3>Despedidos</h3>";
+    echo "<table border = 1>
+    <tr>
+        <th>No.</th>
+        <th>Usuario</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Estado Civil</th>
+        <th>Correo</th>
+        <th>Telefono</th>
+        <th>NSS</th>
+    </tr>";
+    $query = "SELECT * FROM employees where tipo = '1'";
+    $c=1;
+    $result = mysqli_query($conn, $query);
+    while($row = mysqli_fetch_array($result)){
+        $email = $row["email"];
+        $nombre = $row["empfullname"];
+        $apellidos = $row["apellidos"];
+        $username = $row["employid"];
+        $civ = $row["edociv"];
+        $phone = $row["telefono"];
+        $nss = $row["nss"];
+        echo "<tr>
+                <td>$c</td>
+                <td>$username</td>
+                <td>$nombre</td>
+                <td>$apellidos</td>
+                <td>$civ</td>
+                <td>$email</td>
+                <td>$phone</td>
+                <td>$nss</td>
+            </tr>";
+        $c++;
+    }
+    echo "</table>";
+    echo "</br>";
+    mysqli_close($conn);
+
+
+?>
+    
+</body>
+</html>
