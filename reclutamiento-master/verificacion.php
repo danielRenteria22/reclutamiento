60f7192c56ab444ef3bfb929c317b9775dfd59aa
danielRenteria22 <32469635+danielRenteria22@users.noreply.github.com>
2019-01-02 13:10:07 -0700

    Add files via upload

--- reclutamiento-master/verificacion.php
+++ reclutamiento-master/verificacion.php
@@ -8,4 +8,24 @@
             echo "</script>";
         } 
     }
+
+    function verificar_admin(){
+        session_start();
+        if(!isset($_SESSION["id_usuario"])){
+            echo "<script>";
+            echo "alert(\"¡No estas autorizado para entrar aquí!\");\n";
+            echo "window.location.href = \"../../log_in_us.php\"";
+            echo "</script>";
+            
+        } else{
+            if($_SESSION["nivel"] != 3){
+                echo "<script>";
+                echo "alert(\"¡No estas autorizado para entrar aquí!\");\n";
+                echo "window.location.href = \"../../log_in_us.php\"";
+                echo "</script>";
+            }
+        }
+
+        
+    }
 ?>
