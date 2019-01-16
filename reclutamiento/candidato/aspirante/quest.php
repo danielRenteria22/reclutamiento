fe72e96b6a87df3864c6e00b446cf1b6a79cbcd0
danielRenteria22 <32469635+danielRenteria22@users.noreply.github.com>
2019-01-15 17:59:12 -0700

    Add files via upload

--- reclutamiento/candidato/aspirante/quest.php
+++ reclutamiento/candidato/aspirante/quest.php
@@ -1,3 +1,7 @@
+<?php 
+    include "../../verificacion.php";
+    verificar();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Cuestionario de Inicio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../../style.css">
 </head>
 <body>
     <left><h1>Cuestionario Inicial</h1></left>
@@ -24,8 +29,10 @@
             $con=mysqli_connect($host,$user,$pass,$name);
             $result = mysqli_query($con,$query);
             $c=0;
+            $pregunta;
             while($row = mysqli_fetch_array($result)){
                 echo "  <tr>\n";
+                $pregunta[$c] = $row[0];
                 echo "      <br><b><th>Pregunta:</b> ".$row[0]."</th><br/>\n";
                 echo "  <input type='radio' name='rep[$c]' value='Si'checked>&nbsp;Si
                         <input type='radio' name='rep[$c]' value='No'>&nbsp;No</td></tr><br>\n";
@@ -43,35 +50,81 @@
 <?php
     include '../../config.php';
     if(isset($_POST['crear'])){
-        $id = $_GET['idp'];
-        $pr = $_POST['pregunta'];
-        $re = $_POST['rep'];
-        $ds = $_POST['des'];
+        $idu = $_GET['idu'];
+        $idp = $_GET['idp'];
+        $idr = $_GET['idr'];
+        date_default_timezone_set("America/Chihuahua");
+        $fecha = date("Y/m/d");
+        $pasomax;
         $conn=mysqli_connect($host,$user,$pass,$name);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         } 
-        $sql = "INSERT INTO killer_question 
+//insertamos una nueva solicitud
+        mysqli_query($conn,"INSERT INTO solicitudes 
                                 VALUES  (
                                     '',
-                                    '".$id."',
-                                    '".$pr."'
-                                );";
-        if ($conn->query($sql) === TRUE) {
-        //Obtenemos el id de la ponderacion
-        $myid = $conn->insert_id;
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            exit;
-        }
-mysqli_query($conn, "INSERT INTO respuestas 
-                        VALUES  (
+                                    '".$idu."',
+                                    '".$idr."',
+                                    '".$fecha."',
                                     '',
-                                    '".$myid."',
-                                    '".$re."',
-                                    '".$ds."'
+                                    '0',
+                                    0
                                 );");
-        echo"pregunta agregada";        
+                                echo "Holaaaaaaaaa!";
+                                echo mysqli_error($conn);
+//buscamos la solicitud con mayor id (la que se acaba de insertar)
+        $query = "SELECT id FROM solicitudes WHERE id = (SELECT MAX(id) from solicitudes)";
+        $result = mysqli_query($conn,$query);
+        while($row = mysqli_fetch_array($result)){
+            $pasomax=$row[0];
+        }
+        
+//insertamos una calificacion para las encuestas
+        mysqli_query($conn,"INSERT INTO calificaciones 
+                                VALUES  (
+                                    '',
+                                    '',
+                                    '',
+                                    '',
+                                    '$pasomax'
+                                );");
+//isnertar las respuestas de las KQ
+        $a=0;
+        $idresp;
+        $resp = $_POST['rep'];;
+        $querys = "SELECT
+                killer_question.id
+                    FROM 
+                killer_question,
+                respuestas 
+                    WHERE 
+                        $id = id_perfil 
+                    AND
+                        respuestas.id_killer_question=killer_question.id";
+                    $results = mysqli_query($conn,$querys);
+            while($row = mysqli_fetch_array($results)){
+                $val = $resp[$a];
+                $myid = $row[0];
+
+                $eseql = "SELECT id FROM respuestas WHERE id_killer_question = $myid";
+                $rest = mysqli_query($conn,$eseql);
+                while($raw = mysqli_fetch_array($rest))
+                {
+                    $idresp = $raw[0];
+                }
+
+                mysqli_query($conn,"INSERT INTO resp_killer_questions
+                                VALUES(
+                                    '',
+                                    '".$pasomax."',
+                                    '".$myid."',
+                                    '".$idresp."',
+                                    '".$val."'
+                                );");
+                $a++;
+            }
+        echo"Solicitud enviada";        
         $conn->close();
     }
 ?>
